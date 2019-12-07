f017b284172ded4753d5ee9d7323cb4941760eeb
ekaterinakrapivina <binaryrnd@gmail.com>
2019-12-07 22:30:32 +0300

    showSalary func created

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,4 +5,8 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+  filtered = data.filter(elem => elem.age <= age);
+  let res = '';
+  filtered.map(elem => res += elem.name + ', ' + elem.balance + '\n');
+  return res.trim();
 }
