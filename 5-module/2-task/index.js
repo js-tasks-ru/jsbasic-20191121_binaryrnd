7cac95c802f49f35f1a176c1df47997891b90500
ekaterinakrapivina <binaryrnd@gmail.com>
2020-01-09 02:13:16 +0300

    Sortable table component created

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -18,6 +18,32 @@ function SortableTable(items) {
    * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
    */
   this.el = document.createElement('table');
+  
+  let thead = document.createElement('thead');
+  let trHead = document.createElement('tr');
+  
+  thead.appendChild(trHead);
+  this.el.appendChild(thead);
+  
+  for (let key in items[0]) {
+    let td = document.createElement('td');
+    td.innerHTML = key;
+    trHead.appendChild(td);
+  }
+  
+  let tbody = document.createElement('tbody');
+  this.el.appendChild(tbody);
+  
+  for (let person of items) {
+    let tr = document.createElement('tr');
+    //alert(person.name);
+    for (let param in person) {
+      let td = document.createElement('td');
+      td.innerHTML = person[param];
+      tr.appendChild(td);    
+    }
+    tbody.appendChild(tr);
+  } 
 
   /**
    * Метод выполняет сортировку таблицы
@@ -25,5 +51,23 @@ function SortableTable(items) {
    * нужно выполнить сортировку (отсчет начинается от 0)
    * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
    */
-  this.sort = (column, desc = false) => {};
+  this.sort = (column, desc = false) => {
+    let rows = this.el.rows;
+    let arr = [];
+      
+    switch (column) {
+    case 0:
+      arr = Array.from(rows).slice(1).sort((a, b) => (a.cells[column].innerHTML > b.cells[column].innerHTML));
+      break;
+    case 2:
+      arr = Array.from(rows).slice(1).sort((a, b) => (a.cells[column].innerHTML - b.cells[column].innerHTML));
+      break;
+    }
+      
+    if (desc == true) {
+      arr.reverse();
+    }   	      
+    tbody.append(...arr);
+  };
+  
 }
